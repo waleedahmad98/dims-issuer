006a506be3534779f3667a0f22613fd5272eab5f
waleedahmad98 <w.ahmad1024@gmail.com>
2022-05-17 02:57:16 +0500

    bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.js';
 
-// import 'bootstrap/dist/css/bootstrap.css';
-// import'./styles/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import'./styles/style.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
